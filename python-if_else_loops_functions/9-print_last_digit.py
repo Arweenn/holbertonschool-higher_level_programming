390ad582903a682603fcf59bdf1782ce876d27eb
Arweenn <arwendumont25@outlook.fr>
2024-01-24 16:29:52 +0100

    9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,8 +3,6 @@
 def print_last_digit(number):
 
     if number >= 0:
-        last = number % 10
+        return number % 10
     else:
-        last = number % -10
-
-    return last
+        return number % -10
