1b1c9dce8c1c42cfd2fd7872ce0732b6a7c120a3
Arweenn <arwendumont25@outlook.fr>
2024-01-25 16:46:19 +0100

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-from add_0 import add
-
-a = 1
-b = 2
-
-print(f"{a} + {b} = {add(a, b)}")
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    res = add(a, b)
+    print(f"{} + {} = {}".format(a, b, res))
