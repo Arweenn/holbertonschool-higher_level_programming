e4d2444ac47bd56fc209d5539c0b72a55be7b2a2
Arweenn <arwendumont25@outlook.fr>
2024-01-26 12:34:49 +0100

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv) - 1
+    if length > 0:
+        if length == 1:
+            print("{} argument:".format(length))
+        else:
+            print("{} arguments:".format(length))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
+    else:
+        print("{} arguments.".format(length))
