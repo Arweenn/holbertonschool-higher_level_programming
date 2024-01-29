7e855549999e726bac965a31a9960af7338709a8
Arweenn <arwendumont25@outlook.fr>
2024-01-29 17:24:55 +0100

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print(matrix[i][j], end=' ')
+        print()
