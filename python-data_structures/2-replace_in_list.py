395adc5a49b6aa7082a9a39dc5ee5dc7a1e76611
Arweenn <arwendumont25@outlook.fr>
2024-01-29 13:44:50 +0100

    2-replace_in_list.py

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    
+    my_list[idx] = element
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        return my_list
