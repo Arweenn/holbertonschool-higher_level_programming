d4118bd9a2c00731a04ac594f5bbcb8e841005f6
Arweenn <arwendumont25@outlook.fr>
2024-01-29 17:26:08 +0100

    output fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
 
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print(matrix[i][j], end=' ')
+            print("{}".format(matrix[i][j]), end=' ')
         print()
