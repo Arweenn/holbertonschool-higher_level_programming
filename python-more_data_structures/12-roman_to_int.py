d8209306fe33c4b9086e3887874927a9a4659457
Arweenn <arwendumont25@outlook.fr>
2024-02-01 13:37:42 +0100

    task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+
+    r_num = {'I': 1,'V': 5, 'X': 10, 'L': 50,
+             'C': 100, 'D': 500, 'M': 1000}
+
+    num = 0
+    for i in range(len(roman_string)):
+        if i > 0 and r_num[roman_string[i]] > r_num[roman_string[i - 1]]:
+            num += r_num[roman_string[i]] - 2 * r_num[roman_string[i - 1]]
+        else:
+            num += r_num[roman_string[i]]
+    return num
