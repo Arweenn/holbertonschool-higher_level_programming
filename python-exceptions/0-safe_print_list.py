eb8e386f991fce4042e908da0d515c7fa194470c
Arweenn <arwendumont25@outlook.fr>
2024-02-02 20:38:20 +0100

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,13 +2,16 @@
 
 def safe_print_list(my_list=[], x=0):
 
-   try:
-       for i in range(x):
-           print(my_list[i], end="")
+    i = 0
 
-   except IndexError:
-       x = i
+    try:
+        for j in range(x):
+            print("{}".format(my_list[i]), end="")
+            i += 1
 
-   finally:
-       print()
-       return x
+    except IndexError:
+        pass
+
+    finally:
+        print()
+        return i
