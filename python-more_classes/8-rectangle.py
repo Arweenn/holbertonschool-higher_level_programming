f8e3b652632a23fb89e795fd0f90b1aa75395c9c
Arweenn <arwendumont25@outlook.fr>
2024-02-09 14:59:35 +0100

    task 8

---
+++ python-more_classes/8-rectangle.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class Rectangle:
+    """defining a rectangle"""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """This method constructs the object"""
+
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """func defintion"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """func defintion"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """func defintion"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """func defintion"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """func definition"""
+
+        return self.width * self.height
+
+    def perimeter(self):
+        """func definition"""
+
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """func definition"""
+
+        if self.width == 0 or self.height == 0:
+            return ""
+        return "\n".join([str(self.print_symbol) * self.width
+                          for _ in range(self.height)])
+
+    def __repr__(self):
+        """func definition"""
+
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        """func definition"""
+
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """func definition"""
+
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_2 == rect_1:
+            return rect_1 if rect_1.area() >= rect_2.area() else rect_2
