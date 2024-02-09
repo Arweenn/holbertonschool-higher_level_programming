6a2bc3c5bbb88df2fd59903970bd47605842082b
Arweenn <arwendumont25@outlook.fr>
2024-02-09 15:04:47 +0100

    test update

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -11,6 +11,15 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """This method constructs the object"""
 
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self.__width = width
         self.__height = height
         Rectangle.number_of_instances += 1
