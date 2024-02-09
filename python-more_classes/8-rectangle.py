f772ab69ee9b24dccc73eac629c0b88551976655
Arweenn <arwendumont25@outlook.fr>
2024-02-09 15:05:55 +0100

    return fix

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -96,5 +96,4 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if rect_2 == rect_1:
-            return rect_1 if rect_1.area() >= rect_2.area() else rect_2
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
