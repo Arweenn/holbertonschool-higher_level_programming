fc08f451dea64a52805fa6336d1390cd60c4d96e
Arweenn <arwendumont25@outlook.fr>
2024-02-09 11:16:02 +0100

    task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -28,6 +28,6 @@ def matrix_divided(matrix, div):
         for j in range(len(matrix_cpy[i])):
             if isinstance(matrix_cpy[i][j], (int, float)) is False:
                 raise TypeError("matrix must be a matrix \
-                    (list of lists) of integers/floats")
+(list of lists) of integers/floats")
             matrix_cpy[i][j] = round(float(matrix_cpy[i][j]) / div, 2)
     return matrix_cpy
