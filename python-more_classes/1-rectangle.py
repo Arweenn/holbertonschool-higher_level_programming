64870fad86a4e51762e6c6b180b7b4a018d6d61d
Arweenn <arwendumont25@outlook.fr>
2024-02-09 13:18:28 +0100

    task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class Rectangle:
+    """defining a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """This method constructs the object."""
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """func defintion"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+
+    @property
+    def height(self):
+        return self.__height
+
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
