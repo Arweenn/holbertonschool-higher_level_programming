5f9ce1841c204af14eb33426c444f26c1fa7db03
Arweenn <arwendumont25@outlook.fr>
2024-02-13 16:07:32 +0100

    task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Module for BaseGeometry class."""
+"""Start of class base_geometry"""
 
 
 class BaseGeometry:
-    """empty class BaseGeometry."""
-
+    """Create a object"""
     def area(self):
+        """Define the area of an object"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate value."""
-
+        """check if value is correct"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
         if type(value) is not int:
-            raise TypeError(name + " must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
