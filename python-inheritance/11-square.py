a1659543a4970d1d145e307ef0082c385d57f06e
Arweenn <arwendumont25@outlook.fr>
2024-02-13 15:46:27 +0100

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""comment"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class"""
+
+    def __init__(self, size):
+        """init"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
