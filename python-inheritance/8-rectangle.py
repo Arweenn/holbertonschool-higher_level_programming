d378a955c0d58bc06109827ec9e9183365473c81
Arweenn <arwendumont25@outlook.fr>
2024-02-13 15:10:38 +0100

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""comment"""
+
+
+class BaseGeometry:
+    """empty class"""
+
+    def __init__(self, width, height):
+        """comment"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.width = width
+        self.height = height
+
+    def area(self):
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """comment"""
+
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
