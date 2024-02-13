90552a04eb54dfab7acdc1959c1f7f867bcb67a3
Arweenn <arwendumont25@outlook.fr>
2024-02-13 15:40:51 +0100

    Base geo fixing indent

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,6 +11,5 @@ class BaseGeometry:
         """comment"""
         if type(value) is not int:
             raise TypeError(name + " must be an integer")
-
         if value <= 0:
             raise ValueError(name + " must be greater than 0")
