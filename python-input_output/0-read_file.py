11d927a11dbaf55facb0403fac47760ce1aa25c6
Arweenn <arwendumont25@outlook.fr>
2024-02-14 10:51:23 +0100

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,6 @@
 def read_file(filename=""):
     """comment"""
 
-    with open('my_file_0.txt') as f:
+    with open('my_file_0.txt', 'r') as f:
         for line in f:
             print(line, end="")
-    f.close
