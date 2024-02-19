020d6bc5a4c3198aceaee6933cacaf700518168f
Arweenn <arwendumont25@outlook.fr>
2024-02-19 13:57:16 +0100

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,3 +12,35 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         super().__init__(id)
+
+    @property
+    def __width(self):
+        return self.__width
+
+    @__width.setter
+    def __width(self, value):
+        self.__width = value
+
+    @property
+    def __height(self):
+        return self.__height
+
+    @__height.setter
+    def __height(self, value):
+        self.__height = value
+
+    @property
+    def __x(self):
+        return self.__x
+
+    @__x.setter
+    def __x(self, value):
+        self.__x = value
+
+    @property
+    def __y(self):
+        return self.__y
+
+    @__y.setter
+    def __y(self, value):
+        self.__y = value
