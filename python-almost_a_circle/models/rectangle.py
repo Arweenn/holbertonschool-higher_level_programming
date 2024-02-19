f9c3d2015db0c8ceece78b5d515b1dbd4ea4e8c9
Arweenn <arwendumont25@outlook.fr>
2024-02-19 14:31:10 +0100

    task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,6 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        return self.__width * self.__height
