67d0214d77ee723c7418c84181e0ac41ce68aee3
Arweenn <arwendumont25@outlook.fr>
2024-02-19 14:20:37 +0100

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,11 +8,31 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """init class"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
         super().__init__(id)
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x <= 0:
+            raise ValueError("x must be > 0")
+        self.__x = x
+
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y <= 0:
+            raise ValueError("y must be > 0")
+        self.__y = y
 
     @property
     def width(self):
