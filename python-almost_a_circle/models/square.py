aece384bf039cd526e7dbd18f5328b139b783cc4
Arweenn <arwendumont25@outlook.fr>
2024-02-20 12:33:28 +0100

    task 14

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -44,3 +44,7 @@ class Square(Rectangle):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle"""
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
