6b7bdee6839adf36575e62ebcd025f8f9224a440
Arweenn <arwendumont25@outlook.fr>
2024-02-20 11:41:21 +0100

    task 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,17 @@ class Rectangle(Base):
         """overriding method"""
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y}\
  - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+        if args and len(args) > 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
