ea026e6589e966c88c87b767063f1b6f3a795833
Arweenn <arwendumont25@outlook.fr>
2024-03-18 15:35:14 +0100

    task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,4 +7,4 @@ CREATE TABLE second_table
 );
 
 INSERT INTO second_table(id, name, score)
-VALUES (1, 'John', 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+VALUE (1, 'John', 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
