591945e0e17b3281dc4b533fcceaa6dde5fa466b
Arweenn <arwendumont25@outlook.fr>
2024-03-18 14:50:09 +0100

    task 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- creates a table in the current database
-CREATE TABLE IF NOT EXISTS 'first_table'
+CREATE TABLE first_table
 (
-    'id' INT,
-    'name' VARCHAR(256)
-)
+    id INT,
+    name VARCHAR(256)
+);
