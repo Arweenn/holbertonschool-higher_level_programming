7f058a0862ab11f42e5e5a3c936bacbc1a2e8c9b
Arweenn <arwendumont25@outlook.fr>
2024-03-18 15:34:16 +0100

    Task 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- creates a table in the database w/ the addition of content
+CREATE TABLE second_table
+(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table(id, name, score)
+VALUES (1, 'John', 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
