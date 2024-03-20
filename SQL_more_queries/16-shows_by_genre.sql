f165f13e9541cdbf73ae97c24f70daf0cab93da3
Arweenn <arwendumont25@outlook.fr>
2024-03-20 15:50:36 +0100

    task 16

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- lists all shows and all genres linked to that show from the database
+SELECT tv_shows.title, tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_genres.name;
