4bda7a20c2685d29cfdc2d909e4a053494d9c1f2
Arweenn <arwendumont25@outlook.fr>
2024-03-20 15:09:19 +0100

    task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- lists all cities contained in the database
-SELECT cities.id, cities.name, state.name FROM cities
-INNER JOIN states 
-    ON cities.state_id = states.id 
+SELECT cities.id, cities.name, state.name
+FROM cities
+INNER JOIN states
+    ON cities.state_id = states.id
 ORDER BY cities.id ASC;
