5449483608a05acdcac951bba083cbc8a448a632
Arweenn <arwendumont25@outlook.fr>
2024-03-20 14:54:34 +0100

    task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,6 +4,7 @@ USE hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS cities
 (
     id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    state_id INT NOT NULL FOREIGN KEY (state_id) REFERENCES states(id)
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
