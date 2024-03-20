9343d7c93f125eb35eb63f81c853cc8b3c6ef207
Arweenn <arwendumont25@outlook.fr>
2024-03-20 14:27:40 +0100

    update

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS states
 (
     id INT UNIQUE PRIMARY KEY AUTO_INCREMENT,
