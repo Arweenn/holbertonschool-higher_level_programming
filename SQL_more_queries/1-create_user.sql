2b3f4be241f0edbe9e285f0c11923adef7302008
Arweenn <arwendumont25@outlook.fr>
2024-03-20 13:21:08 +0100

    task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creates user 'user_0d_1'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
