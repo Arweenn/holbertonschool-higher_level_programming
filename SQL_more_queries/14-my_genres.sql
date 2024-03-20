450d7a41d4070c33a6451562aa3e0aab32f15131
Arweenn <arwendumont25@outlook.fr>
2024-03-20 15:48:48 +0100

    task 14

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+-- uses the database to list all genres of the show 'DEXTER'
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter" ORDER BY tv_genres.name ASC;
