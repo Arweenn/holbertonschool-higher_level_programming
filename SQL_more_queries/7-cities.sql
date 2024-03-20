e46d3f9421140451d1b36bbf6b5d51d17fb8aeaf
Arweenn <arwendumont25@outlook.fr>
2024-03-20 14:43:33 +0100

    task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS cities
+(
+    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
