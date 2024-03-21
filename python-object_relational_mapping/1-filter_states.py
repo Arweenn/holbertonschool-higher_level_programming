8b58c42d52498cc02613b3e8e372d7bdfb154c41
Arweenn <arwendumont25@outlook.fr>
2024-03-21 10:43:51 +0100

    task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
